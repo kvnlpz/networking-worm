c1428e2afaf1c181b93df88e8ddaebd12892b95a
Kevin L <kevinlopez8554@csu.fullerton.edu>
2021-11-19 19:10:38 -0800

    temporary file creation

---
+++ test1.py
@@ -0,0 +1,42 @@
+# import netifaces
+
+from netifaces import interfaces, ifaddresses, AF_INET
+
+import nmap
+# initialize the port scanner
+nmScan = nmap.PortScanner()
+
+# scan localhost for ports in range 21-443
+nmScan.scan('127.0.0.1', '21-4000')
+nmScan.all_hosts()
+
+
+# kevin
+# interface = netifaces.interfaces()
+# address = netifaces.ifaddresses(interface)
+
+
+
+ip_list = []
+for interface in interfaces():
+	try:
+		for link in ifaddresses(interface)[AF_INET]:
+			ip_list.append(link['addr'])
+			print(ip_list)
+	
+	except KeyError:
+		print("(exception caught) there was an error that i'm ignoring lol")
+
+# print(address)
+
+
+# run a loop to print all the found result about the ports
+for host in nmScan.all_hosts():
+	print('the host : %s (%s)' % (host, nmScan[host].hostname()))
+	print('state : %s' % nmScan[host].state())
+	for proto in nmScan[host].all_protocols():
+		print('Protocol : %s' % proto)
+		lport = nmScan[host][proto].keys()
+		# lport.sort()
+		for port in lport:
+			print ('pOrt : %s\tstate : %s' % (port, nmScan[host][proto][port]['state']))
